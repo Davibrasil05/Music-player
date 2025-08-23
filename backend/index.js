29421931a96e2c9d6d4ccdce124033ce9599db1e
ThiagoMASS <thiago.limamassa2017@gmail.com>
2025-08-23 15:01:12 -0400

    Rota que adiciona o usuario e a musica que ele ouviu, Nome da rota:  Historico

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,17 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+// import jwt from "jsonwebtoken";
 import { createClient } from '@supabase/supabase-js';
 
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT
 
 // Supabase
-const supabaseUrl = "https://fzjetorppylnsrfqkzht.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6amV0b3JwcHlsbnNyZnFremh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU3NDUyMTIsImV4cCI6MjA3MTMyMTIxMn0.bRdcv19s3uUbW-CdrBabnpvfqzuBeMbctwufWueL8N0"; // ⚠️ Use SERVICE_ROLE_KEY no backend
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // JWT secret
@@ -23,19 +23,19 @@ app.use(cors());
 app.use(express.json());
 
 // Middleware de validação JWT
-function validarJWT(req, res, next) {
-  const authHeader = req.headers['authorization'];
-  if (!authHeader) return res.status(401).json({ error: 'Token não fornecido' });
+// function validarJWT(req, res, next) {
+//   const authHeader = req.headers['authorization'];
+//   if (!authHeader) return res.status(401).json({ error: 'Token não fornecido' });
 
-  const token = authHeader.split(' ')[1];
-  if (!token) return res.status(401).json({ error: 'Token mal formatado' });
+//   const token = authHeader.split(' ')[1];
+//   if (!token) return res.status(401).json({ error: 'Token mal formatado' });
 
-  jwt.verify(token, secret, (err, decoded) => {
-    if (err) return res.status(403).json({ error: 'Token inválido ou expirado' });
-    req.user = decoded;
-    next();
-  });
-}
+//   jwt.verify(token, secret, (err, decoded) => {
+//     if (err) return res.status(403).json({ error: 'Token inválido ou expirado' });
+//     req.user = decoded;
+//     next();
+//   });
+// }
 
 // Teste de rota pública
 app.get("/health", (req, res) => {
@@ -74,15 +74,8 @@ app.post("/register", async (req, res) => {
       return res.status(500).json({ error: 'Erro ao criar usuário.' });
     }
 
-    const usuario = data[0];
 
-    const token = jwt.sign(
-      { id: usuario.id, name: usuario.name, email: usuario.email },
-      secret,
-      { expiresIn: '1h' }
-    );
-
-    res.status(201).json({ message: 'Usuário criado com sucesso!', token });
+    res.status(201).json({ message: 'Usuário criado com sucesso!'});
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: 'Erro interno no servidor.' });
@@ -90,7 +83,7 @@ app.post("/register", async (req, res) => {
 });
 
 // Rota protegida (exemplo)
-app.get("/usuarios", validarJWT, async (req, res) => {
+app.get("/usuarios", /*validarJWT*/ async (req, res) => {
   const { data, error } = await supabase.from('users').select('id, name, email');
   if (error) {
     return res.status(500).json({ error: 'Erro ao buscar usuários.' });
@@ -104,6 +97,21 @@ app.get("/musicas",async(req,res)=>{
     return res.status(500).json({error: 'erro ao exibir a lista de musica'})
    }
    res.json(data)
+})
+
+app.post("/historico",async(req,res)=>{
+  const {user_id,song_id} = req.body;
+  
+  const {data,erro} = await supabase
+    .from('music_history')
+    .insert([ { user_id: user_id, song_id: song_id }])
+    .select();
+
+     res.status(201).json({ message: 'Historico criado com sucesso!'});
+})
+
+app.get("/historico/user_id", async(req,res)=>{
+  
 })
 
 
